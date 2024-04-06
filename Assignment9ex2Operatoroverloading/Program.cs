62f279ef8b54a5bb31819f4cb5bb5ed2aa13fc8f
Colin Elliott <79231253+Hexagenia@users.noreply.github.com>
2024-04-06 16:15:05 -0400

    Add files via upload

---
+++ Assignment9ex2Operatoroverloading/Program.cs
@@ -0,0 +1,239 @@
+﻿using System;
+
+namespace Assignment9ex2Operatoroverloading
+{
+    public class Log
+    {
+        public string Activity { get; set; }
+        public double Hours { get; set; }
+        public string? Category { get; set; } //Fun,Work,Other
+
+        // Overloaded operator ++ and operator --
+        public static Log operator ++(Log log)
+        {
+            log.Hours++;
+            return log;
+        }
+
+        public static Log operator --(Log log)
+        {
+            log.Hours--;
+            return log;
+        }
+
+        // Overloaded operator + and operator -
+        public static Log operator +(Log log, int hours)
+        {
+            log.Hours += hours;
+            return log;
+        }
+
+        public static Log operator -(Log log, int hours)
+        {
+            log.Hours -= hours;
+            return log;
+        }
+
+        // Overloaded operator > and operator <
+        public static bool operator >(Log log1, Log log2)
+        {
+            return log1.Hours > log2.Hours;
+        }
+
+        public static bool operator <(Log log1, Log log2)
+        {
+            return log1.Hours < log2.Hours;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Log[] myLog = new Log[100];
+            for (int i = 0; i < myLog.Length; i++)
+            {
+                myLog[i] = new Log();  // creates objects
+            }
+            int selection = Menu();
+            int index = 0, entry = 0;
+            string ans = "";
+            while (selection != 6)
+            {
+                switch (selection)
+                {
+                    case 1:
+                        if (index < 100)
+                        {
+                            Console.Write("Activity Category (Fun, Work, Other): ");
+                            myLog[index].Category = Console.ReadLine();
+                            Console.WriteLine();
+                            Console.Write("Activity: ");
+                            myLog[index].Activity = Console.ReadLine();
+                            Console.WriteLine();
+                            Console.Write("Hours: ");
+                            myLog[index].Hours = double.Parse(Console.ReadLine());
+                            Console.WriteLine();
+                            index++;
+                        }
+                        else
+                            Console.WriteLine("You have too many log entries - see programming");
+                        break;
+                    case 2:
+                        for (int i = 0; i < myLog.Length; i++)
+                        {
+                            if (myLog[i].Activity != "" && myLog[i].Activity != null)
+                            {
+                                Console.WriteLine($"Category: {myLog[i].Category}");
+                                Console.WriteLine($"Activity: {myLog[i].Activity}");
+                                Console.WriteLine($"Hours: {myLog[i].Hours}");
+                            }
+                        }
+                        break;
+                    case 3:
+                        entry = pickEntry(index);
+                        Console.Write("Change Activity Category (Fun, Work, Other) Y for Yes ");
+                        ans = Console.ReadLine();
+                        if (ans == "Y" || ans == "y")
+                        {
+                            Console.Write("Category? ");
+                            myLog[entry].Category = Console.ReadLine();
+                        }
+                        Console.WriteLine();
+                        Console.Write("Change Activity Y for Yes ");
+                        ans = Console.ReadLine();
+                        if (ans == "Y" || ans == "y")
+                        {
+                            Console.Write("Activity: ");
+                            myLog[entry].Activity = Console.ReadLine();
+                        }
+                        Console.WriteLine();
+                        break;
+                    case 4:
+                        entry = pickEntry(index);
+
+                        Console.Write("Increase Hours by 1?  Y for Yes ");
+                        ans = Console.ReadLine();
+                        if (ans == "Y" || ans == "y")
+                        {
+                    
+                            myLog[entry]++;
+                            Console.WriteLine();
+                            break;
+                        }
+
+                        Console.Write("Decrease Hours by 1?  Y for Yes ");
+                        ans = Console.ReadLine();
+                        if (ans == "Y" || ans == "y")
+                        {
+                    
+                            myLog[entry]--;
+                            Console.WriteLine();
+                            break;
+                        }
+
+                        Console.Write("Increase Hours by > 1?  Y for Yes ");
+                        ans = Console.ReadLine();
+                        if (ans == "Y" || ans == "y")
+                        {
+                            Console.Write("Enter the hours: ");
+                            int hr;
+                            while (!int.TryParse(Console.ReadLine(), out hr))
+                                Console.WriteLine($"Please a number");
+                           
+                            myLog[entry] += hr;
+                            Console.WriteLine();
+                            break;
+                        }
+
+                        Console.Write("Decrease Hours by > 1?  Y for Yes ");
+                        ans = Console.ReadLine();
+                        if (ans == "Y" || ans == "y")
+                        {
+                            Console.Write("Enter the hours: ");
+                            int hr;
+                            while (!int.TryParse(Console.ReadLine(), out hr))
+                                Console.WriteLine($"Please a number");
+                   
+                            Console.WriteLine();
+                            break;
+                        }
+
+                        break;
+                    case 5:
+                        double totalFunHours = 0;
+                        double totalWorkHours = 0;
+                        double totalOtherHours = 0;
+
+                        for (int i = 0; i < index; i++)
+                        {
+                            switch (myLog[i].Category)
+                            {
+                                case "Fun":
+                                    totalFunHours += myLog[i].Hours;
+                                    break;
+                                case "Work":
+                                    totalWorkHours += myLog[i].Hours;
+                                    break;
+                                case "Other":
+                                    totalOtherHours += myLog[i].Hours;
+                                    break;
+                            }
+                        }
+
+                        Console.WriteLine("Total Hours by Category");
+                        Console.WriteLine($"Total Fun Hours: {totalFunHours}");
+                        Console.WriteLine($"Total Work Hours: {totalWorkHours}");
+                        Console.WriteLine($"Total Other Hours: {totalOtherHours}");
+
+                        if (totalFunHours > totalWorkHours && totalFunHours > totalOtherHours)
+                        {
+                            Console.WriteLine("The largest number of hours was spent on fun!");
+                        }
+                        else if (totalWorkHours > totalFunHours && totalWorkHours > totalOtherHours)
+                        {
+                            Console.WriteLine("The largest number of hours was spent on work!");
+                        }
+                        else if (totalOtherHours > totalFunHours && totalOtherHours > totalWorkHours)
+                        {
+                            Console.WriteLine("The largest number of hours was spent on other activities!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("The hours spent on different categories are equal!");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("You made an invalid selection, please try again");
+                        break;
+                }
+                selection = Menu();
+
+            }
+        }
+        public static int Menu()
+        {
+            int choice = 0;
+            Console.WriteLine("Please make a selection from the menu");
+            Console.WriteLine("1 - Add an entry");
+            Console.WriteLine("2 - Print All");
+            Console.WriteLine("3 - Change category or activity");
+            Console.WriteLine("4 - Adjust hours");
+            Console.WriteLine("5 - Total categories and compare");
+            Console.WriteLine("6 - Quit");
+            while (!int.TryParse(Console.ReadLine(), out choice))
+                Console.WriteLine("Please select 1 - 6");
+            return choice;
+        }
+        public static int pickEntry(int index)
+        {
+            Console.WriteLine("What entry would you like to change?");
+            Console.WriteLine($"1 through {index}");
+            int entry;
+            while (!int.TryParse(Console.ReadLine(), out entry))
+                Console.WriteLine($"Please select 1 - {index}");
+            entry -= 1;  
+            return entry;
+        }
+    }
+}
---
+++ EvenNumbers/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+namespace EvenNumbers
+{
+    public class EvenNumber
+    {
+        public int Value { get; private set; }
+
+        public EvenNumber(int value)
+        {
+            if (value % 2 != 0)
+            {
+                throw new ArgumentException("Value must be an even number.");
+            }
+            Value = value;
+        }
+
+        // Unary overloaded operators
+        public static EvenNumber operator ++(EvenNumber number)
+        {
+            number.Value += 2; 
+            return number;
+        }
+
+        public static EvenNumber operator --(EvenNumber number)
+        {
+            number.Value -= 2; 
+            return number;
+        }
+
+        public static EvenNumber operator !(EvenNumber number)
+        {
+            number.Value = 0; // Reset to 0
+            return number;
+        }
+
+        
+        public static bool operator ==(EvenNumber number1, EvenNumber number2)
+        {
+            return number1.Value == number2.Value;
+        }
+
+        public static bool operator !=(EvenNumber number1, EvenNumber number2)
+        {
+            return !(number1 == number2);
+        }
+
+        public static bool operator >(EvenNumber number1, EvenNumber number2)
+        {
+            return number1.Value > number2.Value;
+        }
+
+        public static bool operator <(EvenNumber number1, EvenNumber number2)
+        {
+            return number1.Value < number2.Value;
+        }
+
+      
+        public static EvenNumber operator +(EvenNumber number1, EvenNumber number2)
+        {
+            return new EvenNumber(number1.Value + number2.Value);
+        }
+
+        public static EvenNumber operator -(EvenNumber number1, EvenNumber number2)
+        {
+            return new EvenNumber(number1.Value - number2.Value);
+        }
+
+        public static EvenNumber operator *(EvenNumber number1, EvenNumber number2)
+        {
+            return new EvenNumber(number1.Value * number2.Value);
+        }
+
+        public static EvenNumber operator /(EvenNumber number1, EvenNumber number2)
+        {
+            if (number2.Value == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by zero.");
+            }
+            return new EvenNumber(number1.Value / number2.Value);
+        }
+
+        public static EvenNumber operator %(EvenNumber number1, EvenNumber number2)
+        {
+            return new EvenNumber(number1.Value % number2.Value);
+        }
+
+        public override string ToString()
+        {
+            return $"Even Number: {Value}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EvenNumber number1 = new EvenNumber(4);
+            EvenNumber number2 = new EvenNumber(6);
+
+            Console.WriteLine("Original Even Numbers:");
+            Console.WriteLine(number1);
+            Console.WriteLine(number2);
+            Console.WriteLine();
+
+            // Unary operators
+            Console.WriteLine("After using unary operators:");
+            Console.WriteLine(++number1);
+            Console.WriteLine(--number2);
+            Console.WriteLine(!number1); 
+            Console.WriteLine();
+
+            // Comparison operators
+            Console.WriteLine("Comparison Results:");
+            Console.WriteLine($"Are number1 and number2 equal? {number1 == number2}");
+            Console.WriteLine($"Are number1 and number2 not equal? {number1 != number2}");
+            Console.WriteLine($"Is number1 greater than number2? {number1 > number2}");
+            Console.WriteLine($"Is number1 less than number2? {number1 < number2}");
+            Console.WriteLine();
+
+            // Binary operators
+            Console.WriteLine("Binary Operation Results:");
+            Console.WriteLine($"Addition: {number1 + number2}");
+            Console.WriteLine($"Subtraction: {number1 - number2}");
+            Console.WriteLine($"Multiplication: {number1 * number2}");
+            Console.WriteLine($"Division: {number1 / number2}");
+            Console.WriteLine($"Modulus: {number1 % number2}");
+        }
+    }
+}
